e1143fe5d60a951627f09bb2b335d542b9543062
chanti <nirajvarma1928@gmail.com>
2026-02-09 09:51:07 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
+import Home from './Components/HOME/Home'
 function App() {
   
 
   return (
     <>
       <Header/>
+      <Home/>
       <Footer/>
     </>
   )
